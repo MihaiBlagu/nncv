80ed4a2c85daa625a0d7c2fc2214dbf9b75e4791
MihaiBlagu <m.blagu@yahoo.com>
2024-03-19 21:13:06 +0100

    add model_save_path argument

--- run_container.sh
+++ run_container.sh
@@ -7,4 +7,5 @@ wandb login
 
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
          --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes" \
+         --test_data_path "./test_data" \
          --model_save_path "./models"
