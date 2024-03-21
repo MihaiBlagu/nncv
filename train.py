5fb0fe1d44ac6cf0428d799ae0be869aad3dd5c8
MihaiBlagu <m.blagu@yahoo.com>
2024-03-21 16:26:32 +0100

    modify training logic

--- train.py
+++ train.py
@@ -7,8 +7,9 @@ from custom_model import DeepLabV3Plus
 from torchvision.datasets import Cityscapes
 from argparse import ArgumentParser
 
-from utils.process_data import plot_images_with_masks, preprocess, postprocess, preprocess_mask, plot_images_predictions_masks
+from utils.process_data import plot_images_with_masks, preprocess, preprocess_train, preprocess_mask, plot_images_predictions_masks
 from utils.train_test_utils import train, dice_score
+from utils.utils import map_id_to_train_id
 
 from torch.utils.data import DataLoader
 import torch
@@ -16,9 +17,6 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from sklearn.model_selection import KFold
 import random
 import os
-
-import gc
-
 
 
 def get_arg_parser():
@@ -30,23 +28,17 @@ def get_arg_parser():
     return parser
 
 
-def main(args):
+def ex_main(args):
     """define your model, trainingsloop optimitzer etc. here"""
-
-    # set cuda allocation size to 128MB to prevent gpu memory fragmentation
-    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:32'
 
     # get device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     random.seed(42)
 
-    # set torch acllocation size
-    os.environ
-
     # define batch size and epochs
-    batch_size = 16
-    epochs = 10
-    k_folds = 3
+    batch_size = 32
+    epochs = 15
+    k_folds = 5
 
     # data loading
     dataset = Cityscapes(args.data_path, split='train', mode='fine', target_type='semantic')
@@ -64,8 +56,19 @@ def main(args):
     # Define the model and optimizer
     # model = Model().to(device)
     model = DeepLabV3Plus(num_classes=34).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
-    criterion = torch.nn.CrossEntropyLoss(ignore_index=255)
+    
+    # USE THIS WITH KFOLD
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=1e-3)
+
+    # USE THIS WHEN MOVING AWAY FROM KFOLD
+    # # start from lr = (1 - curr_iter/max_iter)^0.9
+    # optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-5)
+    # # step_size = 1 -> decay weight every epoch
+    # # gamma = 0.1 -> deccay factor
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
+
+    # loss
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=255, reduction="mean")
 
     # Loop through each fold
     for fold, (train_idx, val_idx) in enumerate(kf.split(dataset)):
@@ -79,13 +82,14 @@ def main(args):
         # training
         train_losses = []
         val_losses = []
+        val_dices = []
         for epoch in range(1, epochs + 1):
             # shuffle training indices each epoch if desired
             random.shuffle(train_indices)
 
             # train
             model.train()
-            total_loss = 0.0
+            total_train_loss = 0.0
             num_train_batches = len(train_indices) // batch_size
             for i in range(0, len(train_indices), batch_size):
                 # create a batch
@@ -94,10 +98,8 @@ def main(args):
                 for idx in batch_indices:
                     img, mask = dataset[idx]
                     # preproc
-                    img = preprocess(img)
-                    mask = preprocess_mask(mask)
-                    images.append(img)
-                    masks.append(mask)
+                    images.append(preprocess(img))
+                    masks.append(preprocess_mask(mask))
                 images = torch.stack(images).to(device)
                 masks = torch.stack(masks).to(device)
 
@@ -110,13 +112,14 @@ def main(args):
                 loss.backward()
                 optimizer.step()
 
-                total_loss += loss.item()
-            train_losses.append(total_loss / num_train_batches)
+                total_train_loss += loss.item()
+            train_losses.append(total_train_loss / num_train_batches)
             print(f"Epoch {epoch}: Training Loss: {train_losses[-1]:.4f}")
 
             # validation
             model.eval()
             total_val_dice = 0.0
+            total_val_loss = 0.0
             num_val_batches = len(val_indices) // batch_size
             with torch.no_grad():
                 for i in range(0, len(val_indices), batch_size):
@@ -134,91 +137,169 @@ def main(args):
 
                     outputs = model(images)
                     dice = dice_score(outputs, masks.squeeze())
+                    loss = criterion(outputs, masks.squeeze().long())
+
                     total_val_dice += dice.item()
-                val_losses.append(total_val_dice / num_val_batches)
-                print(f"Epoch {epoch}: Validation Dice Score: {val_losses[-1]:.4f}")
+                    total_val_loss += loss.item()
+                val_losses.append(total_train_loss / num_val_batches)
+                val_dices.append(total_val_dice / num_val_batches)
+                print(f"Epoch {epoch}: Validation Loss: {val_losses[-1]:.4f}")
+                print(f"Epoch {epoch}: Validation Dice Score: {val_dices[-1]:.4f}")
 
     # save model
     torch.save(model.state_dict(), os.path.join(args.model_save_path, "deeplabv3plus_ce.pth"))
 
-    # clean gpu memory after training
-    gc.collect()
-    torch.cuda.empty_cache()
-    
+
+def main(args):
+    """define your model, trainingsloop optimitzer etc. here"""
+    # get device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    random.seed(42)
+
+    # define batch size and epochs
+    batch_size = 4
+    epochs = 1
+
+    # data loading
+    trainset = Cityscapes(args.data_path, split='train', mode='fine', target_type='semantic',
+                         transform=preprocess_train, target_transform=preprocess_mask)
+    valset = Cityscapes(args.data_path, split='val', mode='fine', target_type='semantic',
+                        transform=preprocess, target_transform=preprocess_mask)
+
+    # visualize example images
+    # plot_images_with_masks(dataset, indices=[0, 1, 2, 3, 4, 5, 6, 7], num_images_per_row=4,
+    #                        save=False, save_path="./results/plots"    )
+
+    # save original image height and width
+    # img_w, img_h = trainset[0][0].size
+
+    # for testing purposes
+    trainset.images = trainset.images[:8]
+    trainset.targets = trainset.targets[:8]
+    valset.images = valset.images[:8]
+    valset.targets = valset.targets[:8]
+
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+    val_loader = DataLoader(valset, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+
+
+    # Define the model and optimizer
+    model = DeepLabV3Plus(num_classes=34).to(device)
+
+    # start from lr = (1 - curr_iter/max_iter)^0.9
+    optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-5)
+    # step_size = 1 -> decay weight every epoch
+    # gamma = 0.1 -> deccay factor
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
+
+    # loss
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=255)
+
+    # training
+    train_losses = []
+    val_losses = []
+    val_dices = []
+    for epoch in range(1, epochs + 1):
+        # train
+        model.train()
+        total_train_loss = 0.0
+        num_train_batches = len(train_loader)
+        for (images, masks) in train_loader:
+            images, masks = images.to(device), masks.to(device)
+
+            optimizer.zero_grad()
+            # images shape: b, 3, 512, 512
+            outputs = model(images) 
+            # outputs shape: b, 34, 512, 512 -> 1024, 2048, b -> no postproc yet???? also mask resized
+            # outputs = postprocess(outputs, (img_h, img_w))
+            loss = criterion(outputs, masks.squeeze().long())
+            loss.backward()
+            optimizer.step()
+
+            total_train_loss += loss.item()
+        train_losses.append(total_train_loss / num_train_batches)
+        print(f"Epoch {epoch}: Training Loss: {train_losses[-1]:.4f}")
+
+        # validation
+        model.eval()
+        total_val_dice = 0.0
+        total_val_loss = 0.0
+        num_val_batches = len(val_loader)
+        with torch.no_grad():
+            for (images, masks) in val_loader:
+                images, masks = images.to(device), masks.to(device)
+
+                outputs = model(images)
+                dice = dice_score(outputs, masks.squeeze())
+                loss = criterion(outputs, masks.squeeze().long())
+                total_val_dice += dice.item()
+                total_val_loss += loss.item()
+            val_losses.append(total_val_loss / num_val_batches)
+            val_dices.append(total_val_dice / num_val_batches)
+        
+        scheduler.step()
+
+        print(f"Epoch {epoch}: Validation Loss: {val_losses[-1]:.4f}")
+        print(f"Epoch {epoch}: Validation Dice Score: {val_dices[-1]:.4f}")
+
+    # save model
+    torch.save(model.state_dict(), os.path.join(args.model_save_path, "deeplabv3plus_ce.pth"))
+
 
 def test_model(args, model_name="deeplabv3plus_ce.pth"):
-    
-    # set cuda allocation size to 128MB to prevent gpu memory fragmentation
-    os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:32'
-
     # get device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # load model from .pth file
     model = DeepLabV3Plus(num_classes=34).to(device)
-    model.load_state_dict(torch.load(os.path.join(args.model_save_path, model_name)))
+    model.load_state_dict(torch.load(os.path.join(args.model_save_path, model_name), 
+                                    map_location=torch.device(device)))
 
     # define batch size
     batch_size = 4
 
-    # visualize some reslults
-    testset = Cityscapes(args.test_data_path, split='test', mode='fine', target_type='semantic')
+    # load_data
+    testset = Cityscapes(args.data_path, split='test', mode='fine', target_type='semantic',
+                        transform=preprocess, target_transform=preprocess_mask)
+    
+    testset.images = testset.images[:8]
+    testset.targets = testset.targets[:8]
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=True, num_workers=2, drop_last=True)
+
+    # loss
+    criterion = torch.nn.CrossEntropyLoss(ignore_index=255)
 
     # Testing
     model.eval()
     total_test_dice = 0.0
-    num_test_batches = len(testset) // batch_size
-    # maybe these things occupy mem
-    # test_outputs = []
-    # test_masks = []
-    # test_images = []
+    totalt_test_loss = 0.0
+    num_test_batches = len(test_loader)
     with torch.no_grad():
-        for i in range(0, len(testset), batch_size):
-            # Create batches
-            images, masks = [], []
-            for idx in range(i, min(i + batch_size, len(testset))):
-                img, mask = testset[idx]
-                img = preprocess(img)  # Add batch dimension
-                mask = preprocess_mask(mask)  # Add batch dimension
-                images.append(img)
-                masks.append(mask)
-            images = torch.stack(images).to(device)
-            masks = torch.stack(masks).to(device)
+        curr_batch = 0
+        for (images, masks) in test_loader:
+            images = images.to(device)
+            masks = map_id_to_train_id(masks).to(device)
 
             outputs = model(images)
             dice = dice_score(outputs, masks.squeeze())
+            loss = criterion(outputs, masks.squeeze().long())
             total_test_dice += dice.item()
+            totalt_test_loss += loss.item()
 
-            # test_outputs.append(outputs)
-            # test_masks.append(masks)
-            # test_images.append(images)
+            if random.random() < 1.0:
+                # generate 4 random indices in range batch_size
+                indices = random.sample(range(batch_size), 4)
+                plot_images_predictions_masks(images, outputs, masks, 
+                                        indices=range(batch_size), num_images_per_row=2, 
+                                        title=f"Test Batch {curr_batch}", save=True)
             
-            # save batch 3
-            if i == 3 or i == 9 or i == 12:
-                plot_images_predictions_masks(images[i], outputs[i], masks[i], 
-                                    indices=range(len(batch_size)), num_images_per_row=2, 
-                                    title=f"Test Batch {i+1}", save=True)
+            curr_batch += 1
 
+    test_dice_score = total_test_dice / num_test_batches
+    test_loss = totalt_test_loss / num_test_batches
+    print(f"Testing Loss: {test_loss:.4f}")
+    print(f"Testing Dice Score: {test_dice_score:.4f}")
 
-        test_dice_score = total_test_dice / num_test_batches
-        print(f"Testing Dice Score: {test_dice_score:.4f}")
-
-
-    # # Randomly sample two batches
-    # sampled_indices = random.sample(range(len(test_outputs)), 4)
-    # sampled_images = [test_images[idx] for idx in sampled_indices]
-    # sampled_masks = [test_masks[idx] for idx in sampled_indices]
-    # sampled_outputs = [test_outputs[idx] for idx in sampled_indices]
-
-    # # Plot the sampled batches
-    # for i in range(2):  # Assuming you want to plot two batches
-    #     plot_images_predictions_masks(sampled_images[i], sampled_outputs[i], sampled_masks[i], 
-    #                                 indices=range(len(sampled_images[i])), num_images_per_row=2, 
-    #                                 title=f"Test Batch {i+1}", save=False)
-
-    # clean gpu memory after testing
-    gc.collect()
-    torch.cuda.empty_cache()
 
 if __name__ == "__main__":
     # Get the arguments
