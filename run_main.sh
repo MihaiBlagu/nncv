2bf3b4b52f53428f92e1098072d9761036b60001
MihaiBlagu <m.blagu@yahoo.com>
2024-03-21 16:26:01 +0100

    change allowed running time

--- run_main.sh
+++ run_main.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=6:30:00
+#SBATCH --time=10:00:00
 
 cd /gpfs/home4/scur0748/FinalAssignment
 mkdir wandb/$SLURM_JOBID
