4c62896e5587c587b16cd7fe02ec49648ab1422c
Mihai Blagu <scur0748@int6.local.snellius.surf.nl>
2024-03-21 14:14:25 +0100

    modify test_data_path argument to reflect actual location of test set

--- run_container.sh
+++ run_container.sh
@@ -7,5 +7,5 @@ wandb login
 
 torchrun --nnodes=1 --nproc_per_node=1 train.py \
          --data_path "/gpfs/work5/0/jhstue005/JHS_data/CityScapes" \
-         --test_data_path "./test_data" \
+         --test_data_path "./test_data/CityScapes" \
          --model_save_path "./models"
